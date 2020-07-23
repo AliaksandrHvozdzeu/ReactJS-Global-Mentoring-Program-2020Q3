b58b66c328dfdbcb3202aba570816ef07ddf7f39
aliaksandr.hvozdzeu <aliaksandr.hvozdzeu@gmail.com>
2020-07-23 19:36:45 +0300

    Create test components

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import './App.css';
 import HelloWorld from './components/HelloWorld';
 import HelloWorld2 from './components/HelloWorld2';
 
 function App() {
-  const component = React.createElement('p', {}, 'Hello World 3!');
+  const component = React.createElement(
+    'p',
+    {
+      id: 'id_0001',
+      className: 'component',
+    },
+    'Hello World 3!'
+  );
 
   return (
     <div className="App">
