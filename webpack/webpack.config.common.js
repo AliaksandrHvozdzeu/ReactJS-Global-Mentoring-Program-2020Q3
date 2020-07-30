36e3c9c2d346c9dae27e03731daff52beb70d462
aliaksandr.hvozdzeu <aliaksandr.hvozdzeu@gmail.com>
2020-07-30 18:56:17 +0300

    [ERMP]:Refactoring

--- webpack/webpack.config.common.js
+++ webpack/webpack.config.common.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
@@ -27,7 +26,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'public/index.html',
+      template: './public/index.html',
       favicon: "./public/favicon.ico"
     }),
     new MiniCssExtractPlugin(),
